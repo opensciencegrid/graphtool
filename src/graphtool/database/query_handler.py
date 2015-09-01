dbab2338350444dcc967e16ced73873cd281d789
juanfmx2 <juanfmx2@gmail.com>
2015-09-01 04:29:10 -0500

    Fix to Decimal json encoder

--- src/graphtool/database/query_handler.py
+++ src/graphtool/database/query_handler.py
@@ -540,7 +540,7 @@ import decimal
 class DecimalEncoder(json.JSONEncoder):
     def _iterencode(self, o, markers=None):
         if isinstance(o, decimal.Decimal):
-            return float(o)
+            return (str(o) for o in [o])
         return super(DecimalEncoder, self)._iterencode(o, markers)
 
 
